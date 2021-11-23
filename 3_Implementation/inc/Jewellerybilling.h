599434e83c3768b66c6207dfa8b4a9eb4798ba3b
sujith44 <94236917+sujith44@users.noreply.github.com>
2021-11-23 23:35:40 +0530

    Update Jewellerybilling.h

--- 3_Implementation/inc/Jewellerybilling.h
+++ 3_Implementation/inc/Jewellerybilling.h
@@ -6,6 +6,15 @@
 #ifndef _JewelleryBilling_H_
 #define _JewelleryBilling_H_
 
+#include<windows.h>
+#include<stdio.h>
+#include<conio.h>
+#include <stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<dos.h>
+#include<time.h>
+
 
 void dummy(float a)
  
