bb42da29c52fd98e093673fcd9dbfe48ebd3d3bb
sujith44 <94236917+sujith44@users.noreply.github.com>
2021-11-23 23:34:41 +0530

    Create Jewellerybilling.h

---
+++ 3_Implementation/inc/Jewellerybilling.h
@@ -0,0 +1,12 @@
+/** 
+* @file Jewllery Billing.h
+* billing application
+*
+*/
+#ifndef _JewelleryBilling_H_
+#define _JewelleryBilling_H_
+
+
+void dummy(float a)
+ 
+#endif  /* #define _JewelleryBilling_H_  */
