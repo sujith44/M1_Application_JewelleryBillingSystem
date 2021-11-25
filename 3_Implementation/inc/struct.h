7a22bc92883945007336a41d4a8927c3606205b8
sujith44 <94236917+sujith44@users.noreply.github.com>
2021-11-25 10:48:27 +0530

    Update struct.h

--- 3_Implementation/inc/struct.h
+++ 3_Implementation/inc/struct.h
@@ -1,1 +1,45 @@
+#include "Fun.h"
+struct meroDate
+{
 
+int mm,dd,yy;
+
+};
+
+struct staff
+
+{
+
+int id;
+
+char stname[20];
+
+char name[20];
+
+char Material[20];
+
+int Quantity;
+
+int count;
+
+int weight;
+
+int karat;
+
+char *cat;
+
+struct meroDate issued;
+
+struct meroDate duedate;
+
+};
+
+struct staff a;
+
+int main()
+
+{
+Password();
+getch();
+return 0;
+}
