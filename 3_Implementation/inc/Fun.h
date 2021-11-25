89dd39a2eab10a070c80a725f385c098a6524350
sujith44 <94236917+sujith44@users.noreply.github.com>
2021-11-25 10:44:08 +0530

    Update and rename Jewellerybilling.h to Fun.h

---
+++ 3_Implementation/inc/Fun.h
@@ -0,0 +1,25 @@
+/** 
+* @file Fun.h
+* billing application
+*
+*/
+#ifndef _FUN_H_
+#define _FUN_H_
+
+void returnfunc(void);
+void mainmenu(void);
+void additem(void);
+void deleteitem(void);
+void edititem(void);
+void searchitem(void);
+void viewitem(void);
+void closeapplication(void);
+void calculatebill(void);
+void Password();
+void issuerecord();
+void loaderanim();
+
+
+
+ 
+#endif  /* #define _Fun_H_  */
